85eea7e2538779ca96e7466fee5207fcc61e050d
jorgeluis11 <perez.jorge718@gmail.com>
2016-10-29 13:50:10 -0400

    select card without confirmation or fail message

--- app/controllers/card-game-controller.js
+++ app/controllers/card-game-controller.js
@@ -5,9 +5,28 @@ angular.module("starter")
 
     $scope.selectedIndex;
 
-    $scope.cardSelected = function(index){
-        $scope.selectedIndex = index;
+    $scope.cardSelected = function(index)
+    {
         console.log(index);
+        console.log($scope.selectedIndex);
+        if($scope.selectedIndex == undefined)
+            $scope.selectedIndex = index;
+        else if($scope.selectedIndex === index)
+            $scope.selectedIndex = undefined;
+        else if($scope.selectedIndex !== index)
+        {
+            var word1 = $scope.words[$scope.selectedIndex].replace(/(assets\/images\/)|(\.png)|\_| /g,"");
+            var word2 = $scope.words[index].replace(/(assets\/images\/)|(\.png)|\_| /g,"");
+            console.log(word1);
+            console.log(word2);
+            if(word1 === word2){
+                $scope.words[$scope.selectedIndex] = "";
+                $scope.words[index] = "";
+                $scope.selectedIndex = undefined;
+            }else{
+                $scope.selectedIndex = undefined;
+            }
+        }
     }
 
     $scope.imageOrWord = function(word){
